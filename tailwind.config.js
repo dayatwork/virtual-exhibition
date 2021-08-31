abd538b11106ce7ab96f2ae2353ddf69302ca859
Muhammad Hidayatullah <dayatwork095@gmail.com>
2021-08-31 17:21:55 +0700

    add sticky navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ["./dist/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        "-10": "-10",
+      },
+    },
   },
   variants: {
     extend: {},
